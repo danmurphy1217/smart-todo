5218874effc08d898363a908653ceb08d367df4c
Dan Murphy <dan@modernfi.com>
2024-04-24 21:01:36 -0400

    SM integration for google cloud

--- src/backend/integrations/googlecloud/secretsmanager.go
+++ src/backend/integrations/googlecloud/secretsmanager.go
@@ -1,1 +1,44 @@
-package google
+package googlecloud
+
+import (
+	"context"
+	"fmt"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
+)
+
+type GetSecretRequest struct {
+	SecretName string
+}
+type SecretsManager interface {
+	Get(ctx context.Context, req GetSecretRequest) ([]byte, error)
+}
+
+type secretManager struct {
+	client *secretmanager.Client
+}
+
+var _ SecretsManager = &secretManager{}
+
+func NewSecretManager() (SecretsManager, error) {
+	client, err := secretmanager.NewClient(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve secret manager client: %w", err)
+	}
+	return &secretManager{client: client}, nil
+}
+
+func (s *secretManager) Get(ctx context.Context, req GetSecretRequest) ([]byte, error) {
+	secret, err := s.client.AccessSecretVersion(
+		ctx,
+		&secretmanagerpb.AccessSecretVersionRequest{
+			Name: req.SecretName,
+		},
+	)
+	if err != nil {
+		return []byte{}, fmt.Errorf("failed to get secret: %w", err)
+	}
+
+	return secret.Payload.Data, nil
+}
