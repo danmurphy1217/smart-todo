dd4c6e114739052dfc2dfeba969cad9d78ba175d
Dan Murphy <dan@modernfi.com>
2024-04-24 21:01:21 -0400

    models for calendar events

--- src/backend/models/calendar.go
+++ src/backend/models/calendar.go
@@ -1,9 +1,31 @@
-package integrations
+package models
 
-import "time"
+import (
+	"time"
+
+	gcal "google.golang.org/api/calendar/v3"
+)
 
 type Event struct {
 	Title string
 	Start time.Time
 	End   time.Time
+}
+
+func ToEvents(items []*gcal.Event) []Event {
+	var events []Event
+	for _, item := range items {
+		events = append(events, ToEvent(item))
+	}
+	return events
+}
+
+func ToEvent(item *gcal.Event) Event {
+	startTime, _ := time.Parse(time.RFC3339, item.Start.DateTime)
+	endTime, _ := time.Parse(time.RFC3339, item.End.DateTime)
+	return Event{
+		Title: item.Summary,
+		Start: startTime,
+		End:   endTime,
+	}
 }
